c90317e05976c66915fc57a9599159a0bd9e6cd7
Egor Shitikov <egorshitikov@gmail.com>
2024-03-01 00:00:46 -0500

    Update require-config.js

--- view/adminhtml/require-config.js
+++ view/adminhtml/require-config.js
@@ -1,11 +1,13 @@
 
 var config = {
     paths: {
-        gridjs: 'Mage_Gridjs/js/gridjs'
+        gridjs: 'Mage_Gridjs/js/gridjs',
+        DataTable: 'Mage_Gridjs/js/DataTable'
+        //preact: 'Mage_Gridjs/js/preact',
     },
     shim: {
         slick: {
-            deps: ['jquery']
+            deps: ['jquery', 'jquery/ui']
         }
       }
    };
